5eb194ecf7d0f21cd6e904aff7e5771c549f49d7
chogeamos <chogeamos@gmail.com>
2023-04-13 12:40:14 -0700

    helloworld

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_digit(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
